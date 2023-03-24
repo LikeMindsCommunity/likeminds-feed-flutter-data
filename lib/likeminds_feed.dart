c44999cfe3ba8ed5e2fbbe738ef23af3aa28557f
Divyansh Gandhi <120566424+divyanshgandhilm@users.noreply.github.com>
2023-03-24 14:45:17 +0530

    Remove isProd

--- lib/likeminds_feed.dart
+++ lib/likeminds_feed.dart
@@ -11,7 +11,7 @@ import 'package:likeminds_feed/src/methods/methods.dart';
 import 'package:likeminds_feed/src/methods/sdk.dart';
 import 'src/models/models.dart';
 
-const _prod = false;
+const _prod = true;
 
 class LMFeedClient {
   late final SdkApplication _sdkApplication;
@@ -26,7 +26,6 @@ class LMFeedClient {
 
   static LMFeedClient initiateLikeMinds({
     required String apiKey,
-    required bool isProduction,
     required LMSdkCallback sdkCallback,
   }) {
     debugPrint("SDK Initiation point reached");
