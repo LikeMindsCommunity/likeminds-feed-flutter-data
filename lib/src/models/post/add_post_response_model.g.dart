1d66f523ef2c9f3855e6dfa4bfb4d371f5a1867a
TyagiLikeMinds <anurag.tyagi@likeminds.community>
2023-04-04 14:38:12 +0530

    Added Post and User data in PostResponse

--- lib/src/models/post/add_post_response_model.g.dart
+++ lib/src/models/post/add_post_response_model.g.dart
@@ -11,6 +11,18 @@ AddPostResponseEntity _$AddPostResponseEntityFromJson(
     AddPostResponseEntity(
       success: json['success'] as bool,
       errorMessage: json['error_message'] as String?,
+      post: Post.fromEntity(
+        postEntity:
+            PostEntity.fromJson(json['data']['post'] as Map<String, dynamic>),
+      ),
+      users: (json['data']['users'] as Map<String, dynamic>).map(
+        (key, value) => MapEntry(
+          key,
+          PostUser.fromEntity(
+            entity: PostUserEntity.fromJson(value),
+          ),
+        ),
+      ),
     );
 
 Map<String, dynamic> _$AddPostResponseEntityToJson(
@@ -18,4 +30,6 @@ Map<String, dynamic> _$AddPostResponseEntityToJson(
     <String, dynamic>{
       'success': instance.success,
       'error_message': instance.errorMessage,
+      'post': instance.post,
+      'users': instance.users,
     };
