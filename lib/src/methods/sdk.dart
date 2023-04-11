aed5db110635268a7086eab08bd3af5291ad6ded
Divyansh Gandhi <120566424+divyanshgandhilm@users.noreply.github.com>
2023-04-11 19:58:01 +0530

    Adding getDeleteTags

--- lib/src/methods/sdk.dart
+++ lib/src/methods/sdk.dart
@@ -1,9 +1,10 @@
 import 'package:likeminds_feed/src/methods/helper.dart';
 import 'package:likeminds_feed/src/methods/methods.dart';
+import 'package:likeminds_feed/src/methods/moderation.dart';
 import 'package:likeminds_feed/src/repositories/access_repository.dart';
 import 'package:likeminds_feed/src/repositories/auth_repository.dart';
 import 'package:likeminds_feed/src/repositories/helper_repository.dart';
-import 'package:likeminds_feed/src/repositories/media_repository.dart';
+import 'package:likeminds_feed/src/repositories/moderation_repository.dart';
 import 'package:likeminds_feed/src/repositories/post_repository.dart';
 import 'package:likeminds_feed/src/repositories/feed_repository.dart';
 
@@ -49,6 +50,14 @@ class SdkApplication {
     return HelperApi(
       helperRepository: GetIt.instance.get<HelperRepository>(
         instanceName: DIService.kInstanceHelperRepository,
+      ),
+    );
+  }
+
+  ModerationApi getModerationApi() {
+    return ModerationApi(
+      moderationRepository: GetIt.instance.get<ModerationRepository>(
+        instanceName: DIService.kInstanceModerationRepository,
       ),
     );
   }
---
+++ lib/src/models/moderation/delete_reason_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'delete_reason_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DeleteReasonEntity _$DeleteReasonEntityFromJson(Map<String, dynamic> json) =>
+    DeleteReasonEntity(
+      id: json['id'] as int,
+      name: json['name'] as String,
+    );
+
+Map<String, dynamic> _$DeleteReasonEntityToJson(DeleteReasonEntity instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+    };
