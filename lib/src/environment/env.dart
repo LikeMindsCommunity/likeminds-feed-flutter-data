52719c30420c06499777a24cfe682f5f3f286d4b
Divyansh Gandhi <120566424+divyanshgandhilm@users.noreply.github.com>
2023-05-24 02:54:52 +0530

    Update env.dart

--- lib/src/environment/env.dart
+++ lib/src/environment/env.dart
@@ -8,8 +8,6 @@ abstract class EnvDev {
   static const platformCode = _EnvDev.platformCode;
   @EnviedField(varName: 'VERSION_CODE')
   static const int versionCode = _EnvDev.versionCode;
-  // @EnviedField(varName: 'CARAVAN_HOST')
-  // static const caravanHost = _EnvDev.caravanHost;
   @EnviedField(varName: 'KETTLE_HOST')
   static const kettleHost = _EnvDev.kettleHost;
   @EnviedField(varName: 'BUCKET_NAME', obfuscate: true)
@@ -26,8 +24,6 @@ abstract class EnvProd {
   static const platformCode = _EnvProd.platformCode;
   @EnviedField(varName: 'VERSION_CODE')
   static const int versionCode = _EnvProd.versionCode;
-  // @EnviedField(varName: 'CARAVAN_HOST')
-  // static const caravanHost = _EnvProd.caravanHost;
   @EnviedField(varName: 'KETTLE_HOST')
   static const kettleHost = _EnvProd.kettleHost;
   @EnviedField(varName: 'BUCKET_NAME', obfuscate: true)
