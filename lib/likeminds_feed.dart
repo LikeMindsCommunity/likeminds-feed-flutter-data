1ee8882f7c9db81eaa95c3d5ef3881138b3ead6f
Divyansh Gandhi <120566424+divyanshgandhilm@users.noreply.github.com>
2023-05-23 22:10:27 +0530

    Update likeminds_feed.dart

--- lib/likeminds_feed.dart
+++ lib/likeminds_feed.dart
@@ -11,8 +11,7 @@ import 'package:likeminds_feed/src/methods/sdk.dart';
 import 'src/models/models.dart';
 
 /// Flutter flavour/environment manager v0.0.1
-const _prod = false;
-// !bool.fromEnvironment('DEBUG');
+const _prod = !bool.fromEnvironment('DEBUG');
 
 class LMFeedClient {
   late final SDKApplication _sdkApplication;
