b43b2a0d0500da5c7039427d9ffdb5c3ce1468c1
TyagiLikeMinds <anurag.tyagi@likeminds.community>
2024-03-22 12:20:18 +0530

    fix: added user and topic details in comment and reply

--- lib/src/models/comment/add_comment_reply_response.g.dart
+++ lib/src/models/comment/add_comment_reply_response.g.dart
@@ -14,6 +14,20 @@ AddCommentReplyResponseEntity _$AddCommentReplyResponseEntityFromJson(
       reply: CommentEntity.fromJson(
         json['data']['comment'] ?? {},
       ),
+      topics: (json['data']['topics'] as Map<String, dynamic>?)?.map(
+        (k, e) => MapEntry(k, TopicEntity.fromJson(e as Map<String, dynamic>)),
+      ),
+      widgets: (json['data']['widgets'] as Map<String, dynamic>?)?.map(
+        (k, e) =>
+            MapEntry(k, WidgetModelEntity.fromJson(e as Map<String, dynamic>)),
+      ),
+      userTopics: (json['data']['user_topics'] as Map<String, dynamic>?)?.map(
+        (k, e) =>
+            MapEntry(k, (e as List<dynamic>).map((e) => e as String).toList()),
+      ),
+      users: (json['data']['users'] as Map<String, dynamic>?)?.map(
+        (k, e) => MapEntry(k, UserEntity.fromJson(e as Map<String, dynamic>)),
+      ),
     );
 
 Map<String, dynamic> _$AddCommentReplyResponseEntityToJson(
@@ -23,5 +37,9 @@ Map<String, dynamic> _$AddCommentReplyResponseEntityToJson(
       'error_message': instance.errorMessage,
       'data': {
         'comment': instance.reply?.toJson(),
+        'topics': instance.topics?.map((k, e) => MapEntry(k, e.toJson())),
+        'widgets': instance.widgets?.map((k, e) => MapEntry(k, e.toJson())),
+        'user_topics': instance.userTopics,
+        'users': instance.users?.map((k, e) => MapEntry(k, e.toJson())),
       }
     };
--- lib/src/models/feed/comment_detail_response.g.dart
+++ lib/src/models/feed/comment_detail_response.g.dart
@@ -13,7 +13,18 @@ GetCommentResponseEntity _$GetCommentResponseEntityFromJson(
       errorMessage: json['error_message'],
       postReplies: CommentEntity.fromJson(
           json['data']['comment'] as Map<String, dynamic>),
-      users: (json['data']['users'] as Map<String, dynamic>).map(
+      topics: (json['data']['topics'] as Map<String, dynamic>?)?.map(
+        (k, e) => MapEntry(k, TopicEntity.fromJson(e as Map<String, dynamic>)),
+      ),
+      widgets: (json['data']['widgets'] as Map<String, dynamic>?)?.map(
+        (k, e) =>
+            MapEntry(k, WidgetModelEntity.fromJson(e as Map<String, dynamic>)),
+      ),
+      userTopics: (json['data']['user_topics'] as Map<String, dynamic>?)?.map(
+        (k, e) =>
+            MapEntry(k, (e as List<dynamic>).map((e) => e as String).toList()),
+      ),
+      users: (json['data']['users'] as Map<String, dynamic>?)?.map(
         (k, e) => MapEntry(k, UserEntity.fromJson(e as Map<String, dynamic>)),
       ),
     );
@@ -25,6 +36,9 @@ Map<String, dynamic> _$GetCommentResponseEntityToJson(
       'error_message': instance.errorMessage,
       'data': {
         'comment': instance.postReplies?.toJson(),
+        'topics': instance.topics?.map((k, e) => MapEntry(k, e.toJson())),
+        'widgets': instance.widgets?.map((k, e) => MapEntry(k, e.toJson())),
+        'user_topics': instance.userTopics,
         'users': instance.users?.map((k, e) => MapEntry(k, e.toJson())),
       }
     };
